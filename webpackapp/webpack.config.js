db92d6303295b8d6ef487e31d62fc109ed0a53e6
Michael Naumenko <agaspher@outlook.com>
2017-05-15 13:40:57 +0300

    split library code from app code

--- webpackapp/webpack.config.js
+++ webpackapp/webpack.config.js
@@ -2,10 +2,16 @@ var path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
-    entry: "./app/index.js",
+    entry: {
+        bundle: "./app/index.js",
+        vendor: "lodash"
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: path.resolve(__dirname, "dist")
     },
-    resolve: { modules: [path.resolve(__dirname, "app"), "node_modules", "bower_components"] }
+    resolve: { modules: [path.resolve(__dirname, "app"), "node_modules", "bower_components"] },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({ name: "vendor" })
+    ]
 };
